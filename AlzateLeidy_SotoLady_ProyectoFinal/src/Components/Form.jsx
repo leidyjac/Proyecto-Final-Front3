2b5ce9cdfdc187d3e6524f8cd0a45cd8769733f2
Lady Soto <lady.sotodavila@gmail.com>
2023-11-25 15:09:06 -0500

    Se crea el formulario y se renderiza en la ruta de contacto

--- AlzateLeidy_SotoLady_ProyectoFinal/src/Components/Form.jsx
+++ AlzateLeidy_SotoLady_ProyectoFinal/src/Components/Form.jsx
@@ -1,13 +1,55 @@
-import React from "react";
-
+import React, { useState } from "react";
 
 const Form = () => {
-  //Aqui deberan implementar el form completo con sus validaciones
+  const [nombre, setNombre] = useState("");
+  const [email, setEmail] = useState("");
+  const [mensaje, setMensaje] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const formatoDeEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (nombre.length <= 4 && !formatoDeEmail.test(email)) {
+      setMensaje(
+        "El nombre de tener almenos 5 letras y el email debe cumplir con el formato de correo electronico"
+      );
+    } else if (nombre.length <= 4) {
+      setMensaje("El nombre de tener almenos 5 letras");
+    } else if (!formatoDeEmail.test(email)) {
+      setMensaje("El email no cumple con el formato de un correo electrónico");
+    } else {
+      setMensaje(
+        "Gracias " + nombre + ", te contactaremos cuanto antes vía mail"
+      );
+    }
+  };
 
   return (
     <div>
-      <form>
+      <h1>Ingresa tus datos</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="nombre">Nombre:</label>
+        <input
+          type="text"
+          placeholder="Ingrese su nombre"
+          id="nombre"
+          value={nombre}
+          onChange={(e) => setNombre(e.target.value)}
+        />
+        <label htmlFor="email">Email:</label>
+        <input
+          type="text"
+          placeholder="Ingrese su email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input type="submit" value="Enviar datos" />
       </form>
+      <br></br>
+      <br></br>
+      {mensaje && <div>{mensaje}</div>}
     </div>
   );
 };
--- AlzateLeidy_SotoLady_ProyectoFinal/src/Routes/Contact.jsx
+++ AlzateLeidy_SotoLady_ProyectoFinal/src/Routes/Contact.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import Form from '../Components/Form'
+import Form from "../Components/Form";
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Contact = () => {
+
   return (
     <div>
-      <h2>Want to know more?</h2>
-      <p>Send us your questions and we will contact you</p>
+      <h2>Deseas tener mayor información</h2>
+      <p>Dejanos tus datos para contactarte</p>
       <Form/>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
